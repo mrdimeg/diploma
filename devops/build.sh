0fbd8e88fa31930b472dd974e81a9fda1b2379d0
Dmitry Rodioshkin <rodioshkin@gmail.com>
2022-12-06 21:42:05 +0300

    Test

--- devops/build.sh
+++ devops/build.sh
@@ -2,6 +2,6 @@
 set -e -x
 
 # Build the image
-echo $GITHUB_TOKEN | docker login ghcr.io -u mrdimeg --password-stdin
+echo $CR_PAT | docker login ghcr.io -u mrdimeg --password-stdin
 docker build -t ghcr.io/mrdimeg/diploma:latest -f devops/Dockerfile .
 docker push ghcr.io/mrdimeg/diploma:latest
