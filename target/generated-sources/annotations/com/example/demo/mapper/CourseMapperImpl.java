a615fb41900fbed788847bceaa71b05ff465cf40
Dmitry Rodioshkin <rodioshkin@gmail.com>
2022-12-06 12:52:51 +0300

    Update to Diploma

--- target/generated-sources/annotations/com/example/demo/mapper/CourseMapperImpl.java
+++ target/generated-sources/annotations/com/example/demo/mapper/CourseMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-06T11:04:23+0300",
+    date = "2022-12-06T12:49:31+0300",
     comments = "version: 1.5.2.Final, compiler: Eclipse JDT (IDE) 1.4.200.v20221012-0724, environment: Java 17.0.5 (Eclipse Adoptium)"
 )
 @Component
