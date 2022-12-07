5572a7646ec658f087f8747a377bf8189053a70b
Dmitry Rodioshkin <rodioshkin@gmail.com>
2022-12-07 13:22:25 +0300

    platform update

--- devops/build.sh
+++ devops/build.sh
@@ -3,5 +3,5 @@ set -e -x
 
 # Build the docker image
 
-docker build --tag ghcr.io/mrdimeg/diploma:latest -f Dockerfile .
+docker build --platform=linux/amd64 --tag ghcr.io/mrdimeg/diploma:latest -f Dockerfile .
 docker push ghcr.io/mrdimeg/diploma:latest
